80681433a55392fa692032f244f0cf4f295d011e
Ahnaf Farhan <ahnaffarhan95@gmail.com>
2023-01-20 10:14:50 +0600

    Add DOM selectors in JS.
    Add DOM Selectors in JS, store them in variables. Deleted 2nd 'book' dummy element. Add cursor:pointer to nav title.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// DOM Selectors
+const bookDisplay = document.getElementById("bookDisplay");
+const bookContainer = document.getElementById("bookContainer");
+const bookTitle = document.getElementById("bookTitle");
+const bookAuthor = document.getElementById("bookAuthor");
+const bookPages = document.getElementById("bookPages");
+const btnAddBook = document.getElementById("addBook");
+const btnLogin = document.getElementById("btnLogin");
+const btnRead = document.getElementById("btnRead");
+const btnRemove = document.getElementById("btnRemove");
